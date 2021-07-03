a21e048914061886beab168c7bb4f6fb13b00721
Sridaran Thoniyil <sri7thon@gmail.com>
2021-07-02 21:17:31 -0400

    Make Parser::parse_string return structured Data
    Rename the old parse_string to parse_string_raw

--- src/kbdx/compiler.rs
+++ src/kbdx/compiler.rs
@@ -1,6 +1,6 @@
 use super::parser::{ Parser, ParseError };
 
 pub fn compile_string(input: &str) -> Result<String, ParseError> {
-    let pairs = Parser::parse_string(input)?;
+    let pairs = Parser::parse_string_raw(input)?;
     Ok(format!("{:#?}", pairs))
 }
--- src/kbdx/parser.rs
+++ src/kbdx/parser.rs
@@ -10,8 +10,57 @@ pub struct Parser;
 pub type ParseError = pest::error::Error<Rule>;
 pub type Pair<'a> = pest::iterators::Pair<'a, Rule>;
 
+use ahash::AHashMap;
+
+enum AccessModifier {
+    Public,
+    Private,
+}
+
+pub type Map<'a, T> = AHashMap<&'a str, T>;
+pub type PairMap<'a> = Map<'a, Pair<'a>>;
+pub type LayerMap<'a> = Map<'a, Layer<'a>>;
+
+pub struct Layer<'a> {
+    parent_name: Option<&'a str>,
+    aliases: Map<'a, (Pair<'a>, AccessModifier)>,
+    keys: PairMap<'a>,
+}
+
+impl<'a> Layer<'a> {
+    fn default() -> Layer<'a> {
+        Layer {
+            parent_name: None,
+            aliases: AHashMap::default(),
+            keys: AHashMap::default(),
+        }
+    }
+}
+
+pub struct Data<'a> {
+    configuration: PairMap<'a>,
+    global_aliases: PairMap<'a>,
+    layers: LayerMap<'a>,
+}
+
+impl<'a> Data<'a> {
+    fn default() -> Data<'a> {
+        Data {
+            configuration: AHashMap::default(),
+            global_aliases: AHashMap::default(),
+            layers: AHashMap::default(),
+        }
+    }
+}
+
 impl Parser {
-    pub fn parse_string<'a>(input: &str) -> Result<Pairs<Rule>, ParseError> {
+    /// Parses the input text and returns the raw Pest output
+    pub fn parse_string_raw<'a>(input: &str) -> Result<Pairs<Rule>, ParseError> {
         Self::parse(Rule::main, input.as_ref())
+    }
+
+    /// Parses the input text and returns it in a more structured format
+    pub fn parse_string<'a>(input: &'a str) -> Result<Data<'a>, ParseError> {
+        unimplemented!()
     }
 }
