b30cffd288373594bd77cdc8bae5ba1da1fa12da
Sridaran Thoniyil <sri7thon@gmail.com>
2021-07-04 16:02:27 -0400

    Process config in Parser

--- src/kbdx/parser.rs
+++ src/kbdx/parser.rs
@@ -61,6 +61,51 @@ impl Parser {
 
     /// Parses the input text and returns it in a more structured format
     pub fn parse_string<'a>(input: &'a str) -> Result<Data<'a>, ParseError> {
-        unimplemented!()
+        let pairs = Self::parse(Rule::main, input.as_ref())?;
+
+        let mut pairs_iter = pairs.into_iter();
+
+        // step 1: parse configuration
+        let configuration_pair = pairs_iter.next().unwrap();
+        let config_map = create_pair_map(configuration_pair);
+
+        eprintln!("Config: {:#?}", config_map);
+
+        Ok(Data::default())
     }
+}
+
+/// Given an `assignment` Pair, parses the assignment into a tuple where the first item is the
+/// lvalue identifier and the second item is the rvalue Pair
+fn try_parse_assignment<'a>(maybe_assignment: Pair<'a>) -> Option<(&'a str, Pair<'a>)> {
+    let input_rule = maybe_assignment.as_rule();
+
+    if input_rule != Rule::assignment {
+        return None;
+    }
+
+    let mut pairs = maybe_assignment.into_inner();
+    let lvalue = pairs.next().expect("The pair must have an lvalue!");
+
+    assert!(matches!(lvalue.as_rule(), Rule::identifier));
+
+    // TODO support lists as lvalues
+    let identifier_name = lvalue.as_str();
+
+    let rvalue = pairs.next().expect("The pair must have an rvalue!");
+
+    Some((identifier_name, rvalue))
+}
+
+/// Given a Pair that contains `assignment`'s, return a PairMap with the format
+/// { "<lvalue>": "<rvalue>" }
+fn create_pair_map<'a>(input: Pair<'a>) -> PairMap<'a> {
+    input
+        .into_inner()
+        .map(try_parse_assignment)
+        // effectively take while they are assignments
+        .take_while(Option::is_some)
+        // remove the Option layer
+        .map(Option::unwrap)
+        .collect()
 }
