19f949e9e11fa34566a276c609f3d969aa6b286f
Sridaran Thoniyil <sri7thon@gmail.com>
2021-07-08 14:06:15 -0400

    Install color_eyre error/panic hooks in main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use color_eyre::{eyre::Result, eyre::WrapErr, Section};
 use kmonadx::cli::CLI;
 use structopt::StructOpt;
 
 use kmonadx::kbdx::{
-    compiler::compile_string, parser::ParseError, parser::Parser
+    compiler::compile_string, parser::Parser
 };
 
-fn main() -> Result<(), ParseError> {
+fn main() -> Result<()> {
     let cli = CLI::from_args();
+
+    // https://github.com/yaahc/color-eyre/issues/83
+    // We do not want the "Backtrace has been omitted" message to display
+    color_eyre::config::HookBuilder::default()
+        .display_env_section(false)
+        .install()?;
 
     for file_name in cli.filenames {
         let file_contents = std::fs::read_to_string(&file_name).unwrap();
