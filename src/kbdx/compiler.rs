d4330d787e52978394aab5144f3d5485cf46acee
Sridaran Thoniyil <sri7thon@gmail.com>
2021-07-15 15:36:36 -0400

    Create parse module in compiler for parsing rules

--- src/kbdx/compiler.rs
+++ src/kbdx/compiler.rs
@@ -1,4 +1,44 @@
-use super::parser::Parser;
+use super::parser::{LazyButton, Pair, Parser, Rule};
+
+mod parse {
+    use super::*;
+
+    fn as_str<'a>(pair: Pair<'a>) -> &'a str {
+        pair.as_str()
+    }
+
+    macro_rules! create_type_parser {
+        ($rule_name:ident, $type:ty, $success_conversion:expr) => {
+            pub fn $rule_name<'a>(input: &Pair<'a>) -> Result<$type, String> {
+                match input.as_rule() {
+                    Rule::$rule_name => Ok($success_conversion(input.clone())),
+                    r => Err(format!(
+                        "expected {}, found {:?}",
+                        stringify!($rule_name),
+                        r
+                    )),
+                }
+            }
+        };
+    }
+
+    create_type_parser!(double_quoted_string, &'a str, as_str);
+    create_type_parser!(single_quoted_string, &'a str, as_str);
+    create_type_parser!(number, usize, |i: Pair<'a>| i
+        .as_str()
+        .parse::<usize>()
+        .expect("Numbers must be parseable as `usize`s."));
+    create_type_parser!(boolean, bool, |i: Pair<'a>| match i
+        .into_inner()
+        .next()
+        .expect("Booleans must have inner rules for their values")
+        .as_rule()
+    {
+        Rule::keyword_true => true,
+        Rule::keyword_false => false,
+        x => unreachable!("Invalid inner boolean type: {:?}", x),
+    });
+}
 
 pub fn compile_string(mut parser: Parser) -> color_eyre::Result<String> {
     let data = parser.parse_string::<()>()?;
