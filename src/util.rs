d0f379ce57aef88d99a01519fcaf59940d872928
Sridaran Thoniyil <sri7thon@gmail.com>
2022-01-13 11:17:26 -0500

    Silence unused import warning in tests module

--- src/util.rs
+++ src/util.rs
@@ -71,6 +71,12 @@ impl StringStack {
 }
 
 mod tests {
+    // the linter reports that this import is unused.
+    // however, there is a bug in the language server that makes the tests appear to fail to
+    // compile when
+    // the line is removed, when the compiler has no problem with it.
+    // to satisfy the language server, this import is preserved and the warning is silenced.
+    #[allow(unused_imports)]
     use super::StringStack;
 
     #[test]
